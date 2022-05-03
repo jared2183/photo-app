8c372b1e1742b81b01b83e936f3aa2c3322f557f
sil <git@sil.dev>
2022-05-03 17:30:45 -0500

    comments

--- views/comments.py
+++ views/comments.py
@@ -1,7 +1,7 @@
 from flask import Response, request
 from flask_restful import Resource
 import json
-from models import db, Comment, Post
+from models import db, Comment, Following, Post
 
 class CommentListEndpoint(Resource):
 
@@ -11,7 +11,40 @@ class CommentListEndpoint(Resource):
     def post(self):
         # create a new "Comment" based on the data posted in the body 
         body = request.get_json()
-        print(body)
+        if not body.get('post_id'):
+            return Response(json.dumps({'message': "'post_id' is required"}), mimetype="application/json", status=400)
+
+        # check if post_id is an int
+        try:
+            id = int(body.get('post_id'))
+        except:
+            return Response(json.dumps({'message': "the post_id parameter is invalid"}), mimetype="application/json", status=400)
+        
+        # check if post_id is valid
+        if not Post.query.get(id):
+            return Response(json.dumps({'message': "the post_id parameter is invalid"}), mimetype="application/json", status=404)
+
+        # check if comment is empty
+        if not body.get('text'):
+            return Response(json.dumps({'message': "the comment parameter is empty"}), mimetype="application/json", status=400)
+
+        print("HEWOPEWOIPEWOIPE")
+        print(id, Post.query.get(id).user_id)
+
+        # Check if user is authorized to comment
+        if not Following.query.filter_by(user_id=self.current_user.id, following_id=Post.query.get(id).user_id).all():
+            return Response(json.dumps({'message': "the post_id parameter is invalid"}), mimetype="application/json", status=404)
+
+
+        new_comment = Comment(
+            user_id=self.current_user.id,
+            post_id=body.get('post_id'),
+            comment=body.get('comment')
+        )
+
+        db.session.add(new_comment)
+        db.session.commit()
+
         return Response(json.dumps({}), mimetype="application/json", status=201)
         
 class CommentDetailEndpoint(Resource):
@@ -20,8 +53,17 @@ class CommentDetailEndpoint(Resource):
         self.current_user = current_user
   
     def delete(self, id):
-        # delete "Comment" record where "id"=id
-        print(id)
+        comment = Comment.query.get(id)
+
+        if not comment:
+            return Response(json.dumps({'message': "id is invalid"}), mimetype="application/json", status=404)
+
+        if comment.user_id != self.current_user.id:
+            return Response(json.dumps({'message': "id is invalid"}), mimetype="application/json", status=404)
+
+        db.session.delete(comment)
+        db.session.commit()
+
         return Response(json.dumps({}), mimetype="application/json", status=200)
 
 
