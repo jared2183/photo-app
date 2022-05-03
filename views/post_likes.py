3fe7b3dfb807a23ff63d3b8d5c0110b0971d1722
sil <git@sil.dev>
2022-05-03 17:30:53 -0500

    likes

--- views/post_likes.py
+++ views/post_likes.py
@@ -1,6 +1,6 @@
 from flask import Response, request
 from flask_restful import Resource
-from models import LikePost, db
+from models import LikePost, Following, Post, db
 import json
 
 class PostLikesListEndpoint(Resource):
@@ -11,8 +11,38 @@ class PostLikesListEndpoint(Resource):
     def post(self):
         # create a new "like_post" based on the data posted in the body 
         body = request.get_json()
-        print(body)
-        return Response(json.dumps({}), mimetype="application/json", status=201)
+
+        # Checks to see if post_id is in body
+        if not body.get('post_id'):
+            return Response(json.dumps({'message': "'post_id' is required"}), mimetype="application/json", status=400)
+
+        # Checks to see if post_id is an int
+        try:
+            id = int(body.get('post_id'))
+        except:
+            return Response(json.dumps({'message': "the post_id parameter is invalid"}), mimetype="application/json", status=400)
+
+        # Checks if post_id is valid
+        if not Post.query.get(id):
+            return Response(json.dumps({'message': "the post_id parameter is invalid"}), mimetype="application/json", status=404)
+
+        # Check if user is authorized to like
+        if not Following.query.filter_by(user_id=self.current_user.id, following_id=Post.query.get(id).user_id).all():
+            return Response(json.dumps({'message': "the post_id parameter is invalid"}), mimetype="application/json", status=404)
+
+        # Checks for dupes
+        if LikePost.query.filter_by(user_id=self.current_user.id, post_id=id).all():
+            return Response(json.dumps({'message': "like already exists"}), mimetype="application/json", status=400)
+
+        new_like = LikePost(
+            user_id=self.current_user.id,
+            post_id=body.get('post_id')
+        )
+
+        db.session.add(new_like)
+        db.session.commit()
+
+        return Response(json.dumps(new_like.to_dict()), mimetype="application/json", status=201)
 
 class PostLikesDetailEndpoint(Resource):
 
@@ -20,9 +50,19 @@ class PostLikesDetailEndpoint(Resource):
         self.current_user = current_user
     
     def delete(self, id):
-        # delete "like_post" where "id"=id
-        print(id)
-        return Response(json.dumps({}), mimetype="application/json", status=200)
+        # delete "like_post" record where "id"=id
+        like = LikePost.query.get(id)
+
+        if not like:
+            return Response(json.dumps({'message': "like does not exist"}), mimetype="application/json", status=404)
+
+        if like.user_id != self.current_user.id:
+            return Response(json.dumps({'message': "you are not authorized to delete this like"}), mimetype="application/json", status=404)
+
+        like.query.filter_by(id=id).delete()
+        db.session.commit()
+
+        return Response(json.dumps({'message': 'Like {0} was successfully deleted'.format(id)}), mimetype="application/json", status=200)
 
 
 
