adf87c06a40e0f01fa617f1ba163e70d3cd9e237
jared2183 <jaredmyang@gmail.com>
2022-05-25 14:09:21 -0500

    posts and likes work

--- static/js/hw04.js
+++ static/js/hw04.js
@@ -1,3 +1,5 @@
+var activeUser = null
+
 const story2Html = story => {
     return `
         <div>
@@ -27,19 +29,170 @@ const profile2Html = profile => {
         </div>    
         `;
 }
+
+// Posts 
+
+const getPosts = () => {
+    console.log("getPosts")
+    console.log("making sure active user is set", activeUser)
+    fetch('/api/posts')
+        .then(response => response.json())
+        .then(posts => {
+            postHTML = ""
+            // loop through first 10 posts
+            posts.slice(0, 10).forEach(post => {
+                post.current_user_like_id = getLikeID(post)
+                console.log(post.current_user_like_id)
+                postHTML += post2Html_Head(post)
+
+                if (post.comments.length > 0) {
+                    postHTML += post2Html_Comment(post.comments[0])
+                }
+
+                if (post.comments.length > 1) {
+                    postHTML += `<button>View all ${post.comments.length} comments</button>`
+                }
+
+                postHTML += post2Html_Footer(post)
+
+                postHTML += "\n"
+            });
+            
+            console.log('getPosts html', posts)
+            document.querySelector('#posts').innerHTML = postHTML;
+        })
+}
+
+const post2Html_Head = post => {
+    return `
+    <div class="card" post_id=${post.id}>
+        <div class="card-header">
+            <h1>${post.user.username}</h1>
+            <h1>···</h1>
+        </div>
+        <img src="${post.image_url}" alt="${post.user.username}'s posted image">
+        <div class="card-body">
+            <div class="card-buttons">
+                <div class="card-left-buttons">
+                    <button class="${post.current_user_like_id ? "fas liked" : "far"} fa-heart" current_user_like_id="${post.current_user_like_id}" onClick=toggleLike(event)></button>
+                    <button class="far fa-comment"></button>
+                    <button class="far fa-paper-plane"></button>
+                </div>
+                <div>
+                    <i class="far fa-bookmark"></i>
+                </div>
+            </div>
+            <h2>${post.likes.length} likes</h2>
+            <div class="card-desc">
+                <h3>${post.user.username}</h3>
+                <p>${post.caption}</p>
+                <button>more</button>
+            </div>
+            <div class="card-comment-list">
+    `
+}
+
+const post2Html_Comment = comment => {
+    return `
+    <div class="card-comment">
+        <h3>${comment.user.username}</h3>
+        <p>${comment.text}</p>
+    </div>
+    `
+}
+
+const post2Html_Footer = post => {
+    return `
+    </div>
+        <h4>${post.display_time}</h4>
+    </div>
+    <form class="card-footer">
+        <i class="far fa-smile"></i>
+        <input type="text" id="comment" placeholder="Add a comment..." aria-label="Add a comment">
+        <button>Post</button>
+    </form>
+    </div>
+    `
+}
+
+function getLikeID(post) {
+    for (let i = 0; i < post.likes.length; i++) {
+        if (post.likes[i].user_id === activeUser.id) {
+            return post.likes[i].id
+        }
+    }
+
+    return 0
+}
+
+const toggleLike = event => {
+    likeID = event.target.getAttribute('current_user_like_id');
+    postID = event.target.parentElement.parentElement.parentElement.parentElement.getAttribute('post_id');
+
+    // console.log(postID)
+
+    if (likeID != '0') { // Remove like
+        fetch("http://127.0.0.1:5000/api/posts/likes/" + likeID, {
+            method: "DELETE",
+            headers: {
+                'Content-Type': 'application/json',
+            }
+        })
+        .then(data => {
+            data.json().then(dataJSON => {
+                if (data.ok) {
+                    console.log("databoy", dataJSON)
+                    event.target.classList.remove('fas')
+                    event.target.classList.remove('liked')
+                    event.target.classList.add('far')
+                    event.target.setAttribute('current_user_like_id', '0')
+                } else {
+                    console.log(dataJSON)
+                }
+            })
+        });
+    } else { // Add like
+        const postData = {
+            "post_id": postID
+        };
+        
+        fetch("http://127.0.0.1:5000/api/posts/likes/", {
+                method: "POST",
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(postData)
+            })
+            .then(data => {
+                data.json().then(dataJSON => {
+                    if (data.ok) {
+                        console.log("databoy", dataJSON)
+                        event.target.classList.add('liked')
+                        event.target.classList.add('fas')
+                        event.target.classList.remove('far')
+                        event.target.setAttribute('current_user_like_id', dataJSON.id)
+                    } else {
+                        console.log(dataJSON)
+                    }
+                })
+            });
+    }
+}
+
 // Follow and Unfollow buttons
 const toggleFollow = event => {
     const elem = event.target;
     const userId = elem.dataset.userId;
 
     if (elem.innerHTML === "Follow") {
-        elem.innerHTML = "Unfollow"
+        elem.innerHTML = "Following..."
+        console.log(elem.dataset)
         createNewFollow(userId, elem)
-    }
-    else {
-        elem.innerHTML = "Follow"
+    } else if (elem.innerHTML === "Unfollow") {
+        elem.innerHTML = "Unfollowing..."
+        console.log(elem.dataset)
         deleteFollower(elem.dataset.followingId, elem)
-    }
+    }               
 }
 
 const createNewFollow = (userId, elem) => {
@@ -88,7 +241,6 @@ const getProfile = () => {
         })
 }
 
-// 
 const getSuggestion = () => {
     fetch('/api/suggestions/')
         .then(response => response.json())
@@ -110,8 +262,19 @@ const displayStories = () => {
 };
 
 const initPage = () => {
-    displayStories();
-    getSuggestion();
+    fetch("http://127.0.0.1:5000/api/profile/", {
+        method: "GET",
+        headers: {
+            'Content-Type': 'application/json',
+        }
+    })
+    .then(response => response.json())
+    .then(data => {
+        activeUser = data;
+        displayStories();
+        getSuggestion();
+        getPosts();
+    });
 };
 
 // invoke init page to display stories:
