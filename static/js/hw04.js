a87db8a50c9ef2d69471302ff8ead7558b3842c8
jared2183 <jaredmyang@gmail.com>
2022-05-26 11:43:57 -0500

    fixed html

--- static/js/hw04.js
+++ static/js/hw04.js
@@ -111,11 +111,13 @@ const post2Html_Head = post => {
 
 const post2Html_Footer = post => {
     return `
+        </div>
         <div class="card-footer">
             <i class="far fa-smile"></i>
             <input type="text" id="comment" placeholder="Add a comment..." aria-label="Add a comment">
             <button class="comment" post_id="${post.id}" onClick=newComment(event)>Post</button>
         </div>
+    </div>
     </div>
     `
 }
